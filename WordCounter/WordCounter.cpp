93a4a3aa4249ce4a794c120df64602078c28ae1e
JARVIS <JARVIS@DESKTOP-VE0JGI1>
2019-04-03 02:30:09 +0300

    added getWordsCount function

--- WordCounter/WordCounter.cpp
+++ WordCounter/WordCounter.cpp
@@ -1,11 +1,48 @@
 #include "pch.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
 
 #include <boost/filesystem.hpp>
 #include <boost/thread.hpp>
 
+int const THREADS_COUNT = 4;
 
+using namespace std;
+
+namespace fs = boost::filesystem;
+
+std::vector<fs::path> getFilesList(fs::path& root)
+{
+	std::vector<fs::path> files;
+
+	fs::recursive_directory_iterator iter(root);
+
+	for (auto& p : iter)
+		if (fs::is_regular_file(p))
+			files.push_back(p);
+
+	return std::move(files);
+}
+
+long getWordsCount(std::vector<fs::path>::iterator begin,
+	std::vector<fs::path>::iterator end)
+{
+	long wordsCount = 0;
+
+	for (auto it = begin; it != end; ++it)
+	{
+		ifstream file(it->string());
+		
+		string in;
+		while (file >> in)
+			++wordsCount;
+		
+	}
+
+	return wordsCount;
+}
 
 int main(int argc, char* argv[])
 {
@@ -21,11 +58,12 @@ int main(int argc, char* argv[])
 
 	fs::path path(argv[1]);
 
-	fs::recursive_directory_iterator iter(path);
+	vector<fs::path> files = getFilesList(path);
 	
-	for (auto& p : iter)
-		if(fs::is_regular_file(p))
-			cout << p << endl;
+	cout << "WORDS COUNT: " << getWordsCount(files.begin(), files.end());
+
+	//for (auto& p : files)
+	//	cout << p << endl;
 
 	return 0;
 }
